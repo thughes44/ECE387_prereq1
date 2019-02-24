a8464fc37149acf500d86244c6e45255ba0294e1
thughes44 <hughest6@miamioh.edu>
2019-02-24 14:39:08 -0500

    Add files via upload

---
+++ SOS_program.ino
@@ -0,0 +1,46 @@
+void setup()  // Assign pin output 
+{  
+  Serial.begin(9600);
+  pinMode(4,OUTPUT); 
+}
+
+void loop() // Loop forever
+{
+  digitalWrite(4,HIGH);
+  delay(200);
+  digitalWrite(4, LOW);
+  delay(200);  
+  digitalWrite(4,HIGH);
+  delay(200);
+  digitalWrite(4, LOW);
+  delay(200);  
+  digitalWrite(4,HIGH);
+  delay(200);
+  digitalWrite(4, LOW);
+  delay(600);  
+  digitalWrite(4,HIGH);
+  delay(600);
+  digitalWrite(4, LOW);
+  delay(600);  
+  digitalWrite(4,HIGH);
+  delay(600);
+  digitalWrite(4, LOW);
+  delay(600); 
+  digitalWrite(4,HIGH);
+  delay(600);
+  digitalWrite(4, LOW);
+  delay(600); 
+  digitalWrite(4,HIGH);
+  delay(200);
+  digitalWrite(4, LOW);
+  delay(200);  
+  digitalWrite(4,HIGH);
+  delay(200);
+  digitalWrite(4, LOW);
+  delay(200);  
+  digitalWrite(4,HIGH);
+  delay(200);
+  digitalWrite(4, LOW);
+  delay(2000); 
+}
+
